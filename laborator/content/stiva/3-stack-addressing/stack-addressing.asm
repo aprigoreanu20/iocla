e10f53dfa44393b51b0fb9c3d959089ec136f966
Alexandra Prigoreanu <aprigoreanu@stud.acs.upb.ro>
2024-04-29 23:20:35 +0300

    Lab 8: Solution for ex 3 Printed the string, and then its elements (byte by byte)
    Signed-off-by: Alexandra Prigoreanu <aprigoreanu@stud.acs.upb.ro>

--- laborator/content/stiva/3-stack-addressing/stack-addressing.asm
+++ laborator/content/stiva/3-stack-addressing/stack-addressing.asm
@@ -8,27 +8,58 @@ extern printf
 global main
 main:
     mov ebp, esp
-
-    ; TODO 1: replace every push by an equivalent sequence of commands (use direct addressing of memory. Hint: esp)
     mov ecx, NUM
 push_nums:
-    push ecx
+    sub esp, 4
+    mov dword [esp], ecx
     loop push_nums
 
-    push 0
-    push "mere"
-    push "are "
-    push "Ana "
+    sub esp, 4
+    mov dword [esp], 0
+
+    sub esp, 4
+    mov dword [esp], "mere"
+
+    sub esp, 4
+    mov dword [esp], "are "
+
+    sub esp, 4
+    mov dword [esp], "Ana "
 
     lea esi, [esp]
     PRINTF32 `%s\n\x0`, esi
 
     ; TODO 2: print the stack in "address: value" format in the range of [ESP:EBP]
     ; use PRINTF32 macro - see format above
+    mov eax, ebp
+print_stack_label:
+    cmp eax, esp
+    je out_loop
+    PRINTF32 `0x\x0`
+    PRINTF32 `%x\x0`, eax
+    PRINTF32 `: 0x\x0`
+    PRINTF32 `%d\n\x0`, [eax]
+    sub eax, 4
+    jmp print_stack_label
 
+out_loop:
     ; TODO 3: print the string
+    mov eax, esp
+    PRINTF32 `%s\n\x0`, eax
 
     ; TODO 4: print the array on the stack, element by element.
+    mov eax, ebp
+print_element_by_element:
+    cmp eax, esp
+    je exit_loop
+    PRINTF32 `0x\x0`
+    PRINTF32 `%x\x0`, eax
+    PRINTF32 `: 0x\x0`
+    PRINTF32 `%d\n\x0`, [eax]
+    sub eax, 1
+    jmp print_element_by_element
+
+exit_loop:
 
     ; restore the previous value of the EBP (Base Pointer)
     mov esp, ebp

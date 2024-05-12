4fd6f08090cc1df9830d6d701fcaeb38f3d165aa
Alexandra Prigoreanu <aprigoreanu@stud.acs.upb.ro>
2024-05-12 19:34:09 +0300

    Lab 9: Ex 5
    Signed-off-by: Alexandra Prigoreanu <aprigoreanu@stud.acs.upb.ro>

--- laborator/content/apel-functii/5-print-rev-string/print-rev-string.asm
+++ laborator/content/apel-functii/5-print-rev-string/print-rev-string.asm
@@ -2,12 +2,36 @@
 
 section .data
     mystring db "This is my string", 0
+    store_string times 64 db 0
 
 section .text
 
 extern puts
 extern printf
 global main
+
+reverse_string:
+    push ebp
+    mov ebp, esp
+
+    mov eax, dword [ebp+8]
+    mov ecx, dword [ebp+12]
+    add eax, ecx
+    dec eax
+    mov edx, dword [ebp+16]
+
+copy_one_byte:
+    mov bl, byte [eax]
+    mov byte [edx], bl
+    dec eax
+    inc edx
+    loopnz copy_one_byte
+
+    inc edx
+    mov byte [edx], 0
+
+    leave
+    ret
 
 main:
     push ebp
@@ -24,7 +48,16 @@ test_one_byte:
     jmp test_one_byte
 
 out:
-    PRINTF32 `[before]: %s\n[after]: \x0`, mystring
+    mov eax, mystring
+    mov edx, store_string
+
+    push edx
+    push ecx
+    push eax
+    call reverse_string
+    add esp, 12
+
+    PRINTF32 `[before]: %s\n[after]: %s\x0`, mystring, store_string
 
     leave
     ret
